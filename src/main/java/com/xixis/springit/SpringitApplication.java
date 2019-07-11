811eee90c9ace53a005737dcc48ad0eaef25b574
Anthony Alexis Adoasi <anthony.adoasi@callenssolutions.com>
2019-07-11 16:42:05 +0000

    Created our entities and refactored using Project Lombok

--- src/main/java/com/xixis/springit/SpringitApplication.java
+++ src/main/java/com/xixis/springit/SpringitApplication.java
@@ -3,11 +3,15 @@ package com.xixis.springit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringitApplication {
+
+//  private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
 
   public static void main(String[] args) {
     SpringApplication.run(SpringitApplication.class, args);
   }
+
 
 }
